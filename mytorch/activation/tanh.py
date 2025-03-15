16d5898ae8e64a4dfd2bee1ebccd195652de4cef
= <=>
2025-03-15 22:16:19 +0330

    Sigmoid and Tanh functions implemented

--- mytorch/activation/tanh.py
+++ mytorch/activation/tanh.py
@@ -1,9 +1,20 @@
 import numpy as np
 from mytorch import Tensor, Dependency
+from . import sigmoid
 
 def tanh(x: Tensor) -> Tensor:
     """
     TODO: (optional) implement tanh function
     hint: you can do it using function you've implemented (not directly define grad func)
     """
-    pass
+    data = 2 * sigmoid(2* x.data) -1
+    req_grad = x.requires_grad
+
+    if req_grad:
+        def grad_fn(grad: np.ndarray):
+            return grad * 1- pow(data, 2)   
+
+        depends_on = [Dependency(x, grad_fn)]
+    else:
+        depends_on = []
+    return Tensor(data=data, requires_grad=req_grad, depends_on=depends_on)
